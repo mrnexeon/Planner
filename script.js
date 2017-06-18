a4b5f181d00fb8e96759bf8a3ab51cea980ceb1e
Andrew Arkhipow <the.nexeon@gmail.com>
2017-06-18 23:40:03 +0300

    Add script to task writing

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
 var main = function() {
+
     $('button[name="hide"]').on("click", function() {
         if ($(this).hasClass("hidden")) {
-            $(this).removeClass("hidden").parent().find(".card-content").slideDown();
+            $(this).removeClass("hidden").parent().find(".card-content").slideDown(300);
         } else {
-            $(this).addClass("hidden").parent().find(".card-content").slideUp();
+            $(this).addClass("hidden").parent().find(".card-content").slideUp(300);
         }
+    });
+
+    $('button[name="add-task"]').on("click", function() {
+        $(this).parent().find(".add-task-form").slideDown(300);
+    });
+
+    $('button[name="cancel"]').on("click", function() {
+        $(this).parent().slideUp(300);
     });
 }
 
