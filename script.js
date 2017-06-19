d6193e13b578d331e4af62856d85dfb20bd96bbb
Andrew Arkhipow <the.nexeon@gmail.com>
2017-06-19 20:35:11 +0300

    some fix + new style

--- script.js
+++ script.js
@@ -9,11 +9,14 @@ var main = function() {
     });
 
     $('button[name="add-task"]').on("click", function() {
-        $(this).parent().find(".add-task-form").slideDown(300);
+        var $task = $('<li><i class="material-icons">radio_button_unchecked</i><input name="task-text"><time>в течение дня</time></li>');
+        $(this).parent().find("ul").append($task);
+        $task.children('input[name="task-text"]').focus();
     });
 
-    $('button[name="cancel"]').on("click", function() {
-        $(this).parent().slideUp(300);
+     $('ul').on("blur", 'input[name="task-text"]', function() {
+         var text = $(this).val();
+        $(this).replaceWith("<span>" + text + "</span>").after('<button name="edit-task"><i class="material-icons">edit</i></button><button name="remove-task"><i class="material-icons">delete</i></button>');
     });
 }
 
